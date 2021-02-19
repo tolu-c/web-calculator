1e8ad97203b6625192c78d97fcdc735a7f023515
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-02-19 14:55:19 +0100

    testing

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+let questionDisplayBox = document.querySelector('#question');
+let answerDisplayBox = document.querySelector('#answer');
+
+// calls all numbers
+
+let allNum = document.querySelectorAll('.num');
+console.log(allNum);
