948fd9c906b8478d0fdf1071b3bcf9a64c38039c
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-02-22 16:31:55 +0100

    changed js
    testing output

--- static/js/index.js
+++ static/js/index.js
@@ -1,108 +1,110 @@
-let questionDisplayBox = document.querySelector('#question');
-let answerDisplayBox = document.querySelector('#answer');
+let questionDisplayBox = document.querySelector("#question");
+let answerDisplayBox = document.querySelector("#answer");
 let display = [];
-
-
-// backspace function
-
-let backspace = document.getElementById('backspace');
-backspace.addEventListener('click', backSpace);
-function backSpace() {
-    display.pop();
-    let arrLen = display.length;
-    if (arrLen === 0) {
-        let displayZero = questionDisplayBox.innerHTML = 0;
-        return displayZero;
-    }
-    let backSpaceNum = questionDisplayBox.innerHTML = display.join('');
-    return backSpaceNum
-}
-
 
 // clear function
 
-let clear = document.getElementById('clear');
-clear.addEventListener('click', clearQueAndAns);
+let clear = document.querySelector("#clear");
+clear.addEventListener("click", clearQueAndAns);
 function clearQueAndAns() {
-    display = [];
-    console.log(display);
-    questionDisplayBox.innerHTML = 0;
-    answerDisplayBox.innerHTML = 0;
+  display = [];
+  console.log(`display box is ${display}`);
+  questionDisplayBox.innerHTML = 0;
+  answerDisplayBox.innerHTML = 0;
 }
 
+// backspace function
+
+let backspace = document.getElementById("backspace");
+backspace.addEventListener("click", backSpace);
+function backSpace() {
+  display.pop();
+  console.log(`new array is [${display}]`);
+  let arrLen = display.length;
+  if (arrLen === 0) {
+    let displayZero = (questionDisplayBox.innerHTML = 0);
+    return displayZero;
+  }
+  let backSpaceNum = (questionDisplayBox.innerHTML = display.join(""));
+  return backSpaceNum;
+}
 
 // calls all numbers
 
-let allNum = document.querySelectorAll('.num');
-console.log(allNum);
+let allNum = document.querySelectorAll(".num");
+//console.log(allNum);
 
-allNum.forEach(num => {
-    num.addEventListener('click', () => {
-        let actualNum = num.innerHTML;
-        console.log(actualNum);
-        display.push(actualNum);
-        questionDisplayBox.innerHTML = display.join('');
-        console.log(display);
-        let s = String(display);
-        console.log(s);
-        s.split(',');
-        console.log('the length of s is :',s.length);
-        let f = parseFloat(s)
-        console.log(f);
-        
-    })
-})
-
+allNum.forEach((num) => {
+  num.addEventListener("click", () => {
+    let actualNum = num.textContent;
+    console.log(actualNum);
+    //questionDisplayBox.textContent = actualNum;
+    display.push(actualNum);
+    questionDisplayBox.textContent = display.join("");
+    //console.log(display);
+    let s = String(display.join(""));
+    let n = Number(s);
+    //console.log(`${n} + 2 is `,n+2);
+  });
+});
 
 // calls all signs
 
-let allSigns = document.querySelectorAll('.signs');
-allSigns.forEach(sign => {
-    sign.addEventListener('click', () => {
-        let actualSign = sign.innerHTML;
-        console.log(actualSign);
-        display.push(actualSign);
-        questionDisplayBox.innerHTML = display.join('');
-        switch (actualSign) {
-            case '<ion-icon name="close" role="img" class="md hydrated" aria-label="close"></ion-icon>':
-                console.log('you click a multiply sign');
-                // add function
-                function add(num1, num2) {
-                    console.log(num1+num2);
-                }
-                add(display[0],display[2]);
-                break;
-            case '<ion-icon name="remove" role="img" class="md hydrated" aria-label="remove"></ion-icon>':
-                console.log('you click a minus sign');
-                break;
-            case '<ion-icon name="add" role="img" class="md hydrated" aria-label="add"></ion-icon>':
-                console.log('you click a add sign');
-                let a = display.indexOf(actualSign);
-                let beforeA = a - 1;
-                let afterA = a + 1;
-                console.log(display[afterA]);
-                let indexBeforeA = display[beforeA];
-
-                console.log(display[beforeA]);
-                break;
-            case '/':
-                console.log('you click a divide sign');
-                break;
-            default:
-                break;
+let allSigns = document.querySelectorAll(".signs");
+allSigns.forEach((sign) => {
+  sign.addEventListener("click", () => {
+    let actualSign = sign.textContent;
+    display.push(actualSign);
+    questionDisplayBox.textContent = display.join("");
+    console.log(`you clicked ${actualSign}`);
+    let indexOfSign = display.indexOf(actualSign);
+    let x = Number(display.slice(0, indexOfSign).join(''));
+    let lastIndexOfSign = display.lastIndexOf(actualSign);
+    let y = display.slice(indexOfSign+1, lastIndexOfSign)
+    let result;
+    let removeArray;
+    switch (actualSign) {
+      case "×":
+        console.log("you click a multiply sign");
+        break;
+      case "-":
+        console.log("you click a minus sign");
+        break;
+      case "+":
+        console.log("you click a add sign");
+        console.log(`orginal array`, display);
+        console.log(x);
+        console.log(`new array`, display);
+        if (y.length != 0) {
+            console.log(y)
         }
-        console.log(display);
-        console.log(String(display));
-    })
-})
-
-
-// equals function
-
-let eq = document.querySelector('#equals');
-eq.addEventListener('click', equal);
-
-function equal() {
-    console.log(String(display));
-}
-
+        else {
+            console.log(`y is empty`)
+        }
+        result = x + Number(y);
+        console.log(`sum is ${result}`);
+        removeArray = display.splice(0,lastIndexOfSign);
+        removeArray = String(result);
+        display.unshift(removeArray);
+        answerDisplayBox.innerHTML = result;
+        break;
+      case "/":
+        console.log("you click a divide sign");
+        if (y.length != 0) {
+            console.log(y)
+        }
+        else {
+            console.log(`y is empty`)
+        }
+        result = x / Number(y);
+        console.log(`division is ${result}`);
+        removeArray = display.splice(0,lastIndexOfSign);
+        removeArray = String(result);
+        display.unshift(removeArray);
+        answerDisplayBox.innerHTML = result;
+        break;
+      default:
+        break;
+    }
+  });
+});
