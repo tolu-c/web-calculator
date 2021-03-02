97157aee94f7b1b4510629826745a0976c71cd3d
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-03-02 01:25:32 +0100

    update files

--- static/js/index.js
+++ static/js/index.js
@@ -1,110 +1,136 @@
-let questionDisplayBox = document.querySelector("#question");
-let answerDisplayBox = document.querySelector("#answer");
-let display = [];
+//  global variables
+
+let answerBox = document.querySelector("#answer");
+let questionBox = document.querySelector("#question");
+let containerArr = new Array();
+let numArray = new Array();
+let clear = document.querySelector("#clear");
+let backspace = document.querySelector("#backspace");
+let eq = document.querySelector("#equals");
+const allNum = document.querySelectorAll(".num");
+const allSigns = document.querySelectorAll(".signs");
+let allSignsArray = Object.values(allSigns);
+let c = allSignsArray.map((b) => b.textContent);
+let num1, num2;
+
+let num, actualNum, sign, actualSign, x;
+
+// console.log(allSigns);
+
+// eventListener
+
+clear.addEventListener("click", clearFunc);
+backspace.addEventListener("click", backspaceFunc);
+eq.addEventListener("click", equals);
+
+// functions
 
 // clear function
-
-let clear = document.querySelector("#clear");
-clear.addEventListener("click", clearQueAndAns);
-function clearQueAndAns() {
-  display = [];
-  console.log(`display box is ${display}`);
-  questionDisplayBox.innerHTML = 0;
-  answerDisplayBox.innerHTML = 0;
+function clearFunc() {
+  questionBox.textContent = 0;
+  answerBox.textContent = 0;
+  containerArr = [];
+  numArray = [];
 }
 
 // backspace function
-
-let backspace = document.getElementById("backspace");
-backspace.addEventListener("click", backSpace);
-function backSpace() {
-  display.pop();
-  console.log(`new array is [${display}]`);
-  let arrLen = display.length;
-  if (arrLen === 0) {
-    let displayZero = (questionDisplayBox.innerHTML = 0);
-    return displayZero;
+function backspaceFunc() {
+  numArray.pop();
+  containerArr.pop();
+  answerBox.textContent = numArray.join("");
+  let aL = numArray.length;
+  if (aL > 0) {
+    answerBox.textContent = numArray.join("");
+  } else {
+    answerBox.textContent = 0;
   }
-  let backSpaceNum = (questionDisplayBox.innerHTML = display.join(""));
-  return backSpaceNum;
 }
+// fix the backspace only for the numArray in the answerBox
 
-// calls all numbers
-
-let allNum = document.querySelectorAll(".num");
-//console.log(allNum);
-
+// num functions
 allNum.forEach((num) => {
   num.addEventListener("click", () => {
-    let actualNum = num.textContent;
-    console.log(actualNum);
-    //questionDisplayBox.textContent = actualNum;
-    display.push(actualNum);
-    questionDisplayBox.textContent = display.join("");
-    //console.log(display);
-    let s = String(display.join(""));
-    let n = Number(s);
-    //console.log(`${n} + 2 is `,n+2);
+    // if (num.textContent) {
+    //   actualNum = 0.0;
+    // }
+    actualNum = num.textContent;
+    console.log(`the number clicked is ${actualNum}`);
+    numArray.push(actualNum);
+    containerArr.push(actualNum);
+    let x = numArray.join("");
+    let num1 = Number(x);
+    answerBox.textContent = x;
+    console.log(`the number array is ${numArray}`);
+    console.log(`number is ${x}`);
   });
 });
+// have to fix the '.' shii
 
-// calls all signs
-
-let allSigns = document.querySelectorAll(".signs");
+// signs function
 allSigns.forEach((sign) => {
   sign.addEventListener("click", () => {
-    let actualSign = sign.textContent;
-    display.push(actualSign);
-    questionDisplayBox.textContent = display.join("");
-    console.log(`you clicked ${actualSign}`);
-    let indexOfSign = display.indexOf(actualSign);
-    let x = Number(display.slice(0, indexOfSign).join(''));
-    let lastIndexOfSign = display.lastIndexOf(actualSign);
-    let y = display.slice(indexOfSign+1, lastIndexOfSign)
-    let result;
-    let removeArray;
+    actualSign = sign.textContent;
+    console.log(actualSign);
+    containerArr.push(actualSign);
+
+    // makes it possible that when a sign is clicked before a number has no effect(like that)
+    if (containerArr.lastIndexOf(actualSign) === 0) {
+      if (actualSign === "+" || actualSign === "-") {
+        let x = 0;
+        containerArr.unshift(x);
+      } else if (actualSign === "/" || actualSign === "×") {
+        containerArr.pop();
+        // let x = 1;
+        // containerArr.unshift(x);
+      }
+    }
+
+    let currentIndexOfSign = containerArr.lastIndexOf(actualSign);
+    // let nextIndexOfSign = initialIndexOfSign + 1;
+    let beforeIndexOfSign = currentIndexOfSign - 1;
+    // debugging console
+
+    // console.log(containerArr);
+    // console.log(`the index of ${actualSign} is ${currentIndexOfSign}`);
+    // console.log(`the index before of ${actualSign} is ${beforeIndexOfSign} and its current value is
+    // ${containerArr[beforeIndexOfSign]}`);
+    // console.log(
+    //   `the value before ${actualSign} is ${containerArr[beforeIndexOfSign]}`
+    // );
+    // ensures a sign clicked more than once has no effect
+    if (containerArr[currentIndexOfSign] === containerArr[beforeIndexOfSign]) {
+      console.log(`you hit ${actualSign} twice`);
+      containerArr.pop();
+    }
+    // makes changing of signs possible after a sign has been originally clicked
+    // checks both current and before current index are both signs
+    // and not signs and nums
+    if (containerArr[currentIndexOfSign] !== containerArr[beforeIndexOfSign]) {
+      if (
+        c.includes(containerArr[currentIndexOfSign]) &&
+        c.includes(containerArr[beforeIndexOfSign])
+      ) {
+        console.log("true");
+        containerArr[beforeIndexOfSign] = containerArr[currentIndexOfSign];
+        containerArr.pop();
+        console.log(containerArr);
+      }
+    }
+
     switch (actualSign) {
-      case "×":
-        console.log("you click a multiply sign");
-        break;
-      case "-":
-        console.log("you click a minus sign");
-        break;
       case "+":
-        console.log("you click a add sign");
-        console.log(`orginal array`, display);
-        console.log(x);
-        console.log(`new array`, display);
-        if (y.length != 0) {
-            console.log(y)
-        }
-        else {
-            console.log(`y is empty`)
-        }
-        result = x + Number(y);
-        console.log(`sum is ${result}`);
-        removeArray = display.splice(0,lastIndexOfSign);
-        removeArray = String(result);
-        display.unshift(removeArray);
-        answerDisplayBox.innerHTML = result;
         break;
-      case "/":
-        console.log("you click a divide sign");
-        if (y.length != 0) {
-            console.log(y)
-        }
-        else {
-            console.log(`y is empty`)
-        }
-        result = x / Number(y);
-        console.log(`division is ${result}`);
-        removeArray = display.splice(0,lastIndexOfSign);
-        removeArray = String(result);
-        display.unshift(removeArray);
-        answerDisplayBox.innerHTML = result;
-        break;
+
       default:
         break;
     }
+
+    numArray = [];
+    questionBox.textContent = containerArr.join("");
   });
 });
+
+// equals function
+function equals() {
+  console.log("equals");
+}
